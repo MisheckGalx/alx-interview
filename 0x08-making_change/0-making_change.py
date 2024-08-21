45c36572e6119d5ee07a15805f8c7620dc6fc734
Misheck Gogo <misheckgogo45@gmail.com>
2024-08-21 13:20:05 -0700

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,15 +1,24 @@
+#!/usr/bin/python3
+
+""" Contains makeChange function"""
+
+
 def makeChange(coins, total):
+    """
+    Returns: fewest number of coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any number of coins you have, return -1
+    """
+    if not coins or coins is None:
+        return -1
     if total <= 0:
         return 0
-    
-    # Initialize the dp array with infinity, except for dp[0] which is 0
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-    
-    # Update the dp array
+    change = 0
+    coins = sorted(coins)[::-1]
     for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-    
-    # If dp[total] is still infinity, return -1 (no solution possible)
-    return dp[total] if dp[total] != float('inf') else -1
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
